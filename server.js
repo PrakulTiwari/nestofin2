6f15fa95c2ae6f12b3f39a0bf647bcc2748bced0
Badhatalaf <aryavtiwari2005@gmail.com>
2020-08-29 23:15:27 +0530

    'bhvrbhw4rhy]'

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
     }))
     app.use(morgan('dev'))
 }
-else {
+else if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client-reacts/build'))
     app.use(express.cookieParser('your secret here!'))
     app.use(express.session())
