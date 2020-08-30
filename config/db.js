bf68b1ba5dccb59525852b8f30bd44a2eecdc6dc
Badhatalaf <aryavtiwari2005@gmail.com>
2020-08-30 14:20:40 +0530

    'bgvgbgv'

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+//mongodb://heroku_b0jmp3bz:in4mev1iq46vqo2bo45e5pkicl@ds033046.mlab.com:33046/heroku_b0jmp3bz
 const connectDB = async () => {
     const connection = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
