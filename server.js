88a3011fcac7345b0baf586306f4a3fb72040267
Badhatalaf <aryavtiwari2005@gmail.com>
2020-09-02 23:37:04 +0530

    'commentremoved1'

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 else if (process.env.NODE_ENV === 'production') {
     app.use (express.static(path.join('client-reacts', 'build')));
-    app.use(cors({
-        origin: process.env.CLIENT_URL
-    }))
+    // app.use(cors({
+    //     origin: process.env.CLIENT_URL
+    // }))
     app.use(morgan('tiny'))
    
     app.get("/",(req,res)=>{
