e1744ac9c39fa9de2d9918b53ebd49ab52fdaf74
Badhatalaf <aryavtiwari2005@gmail.com>
2020-09-02 23:55:11 +0530

    'commentremoved'

--- server.js
+++ server.js
@@ -33,17 +33,17 @@ if (process.env.NODE_ENV === 'development') {
 }
 else if (process.env.NODE_ENV === 'production') {
     app.use (express.static(path.join('client-reacts', 'build')));
-    // app.use(cors({
-    //     origin: process.env.CLIENT_URL
-    // }))
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }))
     app.use(morgan('tiny'))
    
     app.get("/",(req,res)=>{
         res.sendFile(path.join('client-reacts','client-reacts','build','index.html'));
     })
-    // app.get("/*",(req,res)=>{
-    //     res.sendFile(path.join('client-reacts','client-reacts','build','index.html'));
-    // })
+    app.get("/*",(req,res)=>{
+        res.sendFile(path.join('client-reacts','client-reacts','build','index.html'));
+    })
     
     // app.use(express.cookieParser('your secret here!'))
 //    app.use(express.session())
