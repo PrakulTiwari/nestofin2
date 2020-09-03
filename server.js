da141d60da5f8738792e2e7b848cae97d6656724
Badhatalaf <aryavtiwari2005@gmail.com>
2020-09-03 10:55:47 +0530

    'hell'

--- server.js
+++ server.js
@@ -33,10 +33,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 else if (process.env.NODE_ENV === 'production') {
     app.use (express.static(path.join('client-reacts', 'build')));
-    app.use(cors({
-        origin: process.env.CLIENT_URL
-    }))
-    app.use(morgan('tiny'))
+    app.use(cors());
+    
+    app.use(morgan('tiny'));
    
     app.get("/",(req,res)=>{
         res.sendFile(path.join('client-reacts','client-reacts','build','index.html'));
