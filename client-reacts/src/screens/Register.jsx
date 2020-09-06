a26470361ed5c62a00bd3f7e0cb190410fc66329
Badhatalaf <aryavtiwari2005@gmail.com>
2020-09-06 12:36:28 +0530

    'hell'

--- client-reacts/src/screens/Register.jsx
+++ client-reacts/src/screens/Register.jsx
@@ -66,7 +66,7 @@ const Register = () => {
 
   return (
     <div className='min-h-screen bg-gray-100 text-gray-900 flex justify-center'>
-      {isAuth() ? <Redirect to='/' /> : null}
+      {isAuth() ? <Redirect to='/dashboard' /> : null}
       <ToastContainer />
       <div className='max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1'>
         <div className='lg:w-1/2 xl:w-5/12 p-6 sm:p-12'>
@@ -142,7 +142,7 @@ const Register = () => {
           ></div>
         </div>
       </div>
-      ;
+      
     </div>
   );
 };
