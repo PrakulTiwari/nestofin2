6ce0c93132ea2d33a865c2f9770705eebd4ee5c3
Badhatalaf <aryavtiwari2005@gmail.com>
2020-09-06 21:17:44 +0530

    'hell'

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -48,7 +48,7 @@ exports.registerController = (req, res) => {
             subject: 'Account activation link',
             html: `   
                 <h1>You are just one click away!</h1>
-                <a href=${process.env.CLIENT_URL}/client-reacts/src/screens/activate/${token}/>Click Here!</a>
+                <a href=${process.env.CLIENT_URL}/users/activate/${token}/>Click Here!</a>
                 <hr />
                 <p>This email may containe sensitive information</p>
                 <p>${process.env.CLIENT_URL}</p>
