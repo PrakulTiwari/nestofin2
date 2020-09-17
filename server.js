9ac4fa5143c29e3cfd41d0f55ea018c261f4359f
Badhatalaf <aryavtiwari2005@gmail.com>
2020-09-17 23:14:22 +0530

    'hell'

--- server.js
+++ server.js
@@ -51,6 +51,9 @@ else if (process.env.NODE_ENV === 'production') {
     app.get("/*",(req,res)=>{
         res.sendFile(path.join('client-reacts','build','index.html'))
     })
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join('client-reacts','build','index.html'))
+    })
     
     // app.use(express.cookieParser('your secret here!'))
 //    app.use(express.session())
