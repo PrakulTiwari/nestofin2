9cb2553f1db305b690f36d63516b120cb086c9db
Badhatalaf <aryavtiwari2005@gmail.com>
2020-10-13 00:33:14 +0530

    'hell'

--- config/db.js
+++ config/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 //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
 
 const connectDB = async () => {
-    return mongoose.connect('mongodb://localhost:27017/nestofin', {
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+    return mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true
     });
-    console.log(`MongoDB Connected: ${connection.connection.host}`);
 };
 
 module.exports = connectDB;
