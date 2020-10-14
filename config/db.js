d64f5d61870c4241a88704dc6c25a5e9af8dddc9
Badhatalaf <aryavtiwari2005@gmail.com>
2020-10-14 21:12:02 +0530

    'hell'

--- config/db.js
+++ config/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
-//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
-
+//mongodb://heroku_b0jmp3bz:in4mev1iq46vqo2bo45e5pkicl@ds033046.mlab.com:33046/heroku_b0jmp3bz
 const connectDB = async () => {
-    console.log(`MongoDB Connected: ${connection.connection.host}`);
-    return mongoose.connect(process.env.MONGO_URI, {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true
-    });
-};
+    })
+    .catch(err => console.log(`${err}`));
+
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+}; 
 
 module.exports = connectDB;
-
-
-//process.env.MONGO_URI
