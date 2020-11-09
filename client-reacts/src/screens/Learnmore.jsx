bed19843e90d7febff7b08eb07ac338254721b92
Prakul <prakultiwari28@gmail.com>
2020-11-09 12:34:21 +0530

    'hell'

--- client-reacts/src/screens/Learnmore.jsx
+++ client-reacts/src/screens/Learnmore.jsx
@@ -24,9 +24,9 @@ function Learnmore({ history }) {
           <div className="text-part">
             <h1>About Us</h1>
             <p className="main-win-sub-win-para1">NESTOFIN PVT. LTD. was commenced in July 2020. It is a platform that uses AI algorithm with the help of investment advisors, and seasoned investors to supply the simplest and the best guidance possible on the investment made by the clients. Company focuses on  primary allocation to mutual funds and shares. We propose better written algorithm investment strategies and help within the allocation in low risk instruments. We tend to supply our clients with complete solution in their diversified portfolio creation. Our aim and objective is to realize - healthy beta value in portfolio, therefore giving our clients - return with utter most stability and adaptability. Our target clients are business owners, middle income households, students, youngsters.For college students and youngsters, we start investment guidance with the very bit of Rs 99. We want to serve the unpriviledged and underpowered working class of our country by making them believe in the power of investing and teaching the healthy financial life mantra to all.</p>
-            <p className="main-win-sub-win-para1">Email - nestofin@gmail.com</p>
+            {/* <p className="main-win-sub-win-para1">Email - nestofin@gmail.com</p>
             <p className="main-win-sub-win-para1">Phone Number - 8076131157</p>
-            <p className="main-win-sub-win-para1">Address - 175/16A, Faridabad, Haryana 121002</p>
+            <p className="main-win-sub-win-para1">Address - 175/16A, Faridabad, Haryana 121002</p> */}
 
           </div>
         </div>
