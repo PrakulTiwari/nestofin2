54b5e4f4710be60515a370d58af85378148d3255
Prakul <prakultiwari28@gmail.com>
2020-11-14 17:59:49 +0530

    'hell'

--- client-reacts/src/screens/Investing.jsx
+++ client-reacts/src/screens/Investing.jsx
@@ -20,9 +20,10 @@ function Investing({ history }) {
             {/* <h1>COMING SOON...</h1> */}
             <h1>Buy Nest Egg Alpha</h1>
             <h3>1 Alpha Yolk = Rs100</h3>
-            <h3>The price of Alpha Yolk will be updated on 15th of every month.</h3>
-            <p className="main-win-sub-win-para1">After purchasing your yolk will be deposited in your nest wallet.
-                To withdraw  open nest wallet and withdraw the desired yolks, withdrawal will not be processed on bank holidays.</p>
+            <h3>The price of Alpha Yolk will be </h3>
+            <h3>updated on 15th of every month.</h3>
+            <p className="main-win-sub-win-para1">After purchasing,your yolk will be deposited in your nest wallet.
+                To withdraw,open nest wallet and withdraw the desired yolks, withdrawal will not be processed on bank holidays.</p>
             {/* <p className="main-win-sub-win-para2">Choose your first account to grow your cash or automate your investments.</p>
             <p className="main-win-sub-win-para3">Fund with ease. Get started with $1 for cash and $500 for investment accounts.</p> */}
             <form><script src="https://cdn.razorpay.com/static/widget/payment-button.js" data-payment_button_id="pl_FVeWx1Ay2BVrRa"></script> </form>
